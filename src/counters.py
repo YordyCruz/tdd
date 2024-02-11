33a042b28f0ff8dcd4a01d3308f6bd66d726ef7f
Yordy <yordycruz0329@gmail.com>
2024-02-10 18:41:18 -0800

    Fixing workflow file, and counter file to pass lwith link flake8
    I got error of missing whitespace after keyword, and after ':' and blank line at end of file. I fix it the problem and checking the test build again.

--- src/counters.py
+++ src/counters.py
@@ -1,24 +1,21 @@
 from flask import Flask
-
-# we need to import the unit under test - counter
 from src import status
 
 app = Flask(__name__)
 
 COUNTERS = {}
 
-
 # We will use the app decorator and create a route called slash counters.
-# specify the variable in route <name>
-# let Flask know that the only methods that is allowed to called
+# Specify the variable in route <name>.
+# Let Flask know that the only method that is allowed to be called
 # on this function is "POST".
 @app.route('/counters/<name>', methods=['POST'])
 def create_counter(name):
-    """Create a counter"""
+    """Create a counter."""
     app.logger.info(f"Request to create counter: {name}")
     global COUNTERS
     if name in COUNTERS:
-        return{"Message":f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
+        return {"Message": f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
 
     # If the counter doesn't exist, create it with a value of 0.
     COUNTERS[name] = 0
@@ -26,28 +23,18 @@ def create_counter(name):
 
 @app.route('/counters/<name>', methods=['PUT'])
 def update_counter(name):
-    """Update a counter"""
+    """Update a counter."""
     app.logger.info(f"Request to update counter: {name}")
     global COUNTERS
     if name in COUNTERS:
-        # If the counter exists, increment it by 1
+        # If the counter exists, increment it by 1.
         COUNTERS[name] += 1
         return {name: COUNTERS[name]}, status.HTTP_200_OK
 
-
 @app.route('/counters/<name>', methods=['GET'])
 def read_counter(name):
-    """Read a counter"""
+    """Read a counter."""
     app.logger.info(f"Request to read counter: {name}")
     global COUNTERS
     if name in COUNTERS:
         return {name: COUNTERS[name]}, status.HTTP_200_OK
-
-    #return {"Message": f"Counter {name} does not exists"}, status.HTTP_404_NOT_FOUND
-
-
-
-
-
-
-
